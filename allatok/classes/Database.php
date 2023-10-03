1fb011bcd7b278cef952538c50d198ae513ca99f
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-10-03 10:04:47 +0200

    Add files via upload

--- allatok/classes/Database.php
+++ allatok/classes/Database.php
@@ -4,12 +4,22 @@ class Database {
 
     private $db = null;
 
-    public function __construct($host,  $username, $pass,$db) {
-        $this->db = new mysqli($host,$username, $pass, $db);
+    public function __construct($host, $username, $pass, $db) {
+        $this->db = new mysqli($host, $username, $pass, $db);
     }
 
     public function login($name, $pass) {
-        if ($result = $this->db->query('SELECT * FROM users WHERE name LIKE "'.$name.'";')) {
+        //-- jelezzük a végrehajtandó SQL parancsot
+        $stmt = $this->db->prepare('SELECT * FROM users WHERE name LIKE ?;');
+        //-- elküldjük a végrehajtáshoz szükséges adatokat
+        $stmt->bind_param("s", $name);
+
+        if ($stmt->execute()) {
+            //-- Sikeres végrehajtás után lekérjül az adatokat
+            $result = $stmt->get_result();
+            echo '<pre>';
+            var_dump($result);
+            echo '<pre>';
             echo "Returned rows are: " . $result->num_rows;
             // Free result set
             $result->free_result();
